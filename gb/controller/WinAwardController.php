f1905e3711f093a29f8caf8ca0148e5630a70bae
Stijn23 <stijn17@hotmail.com>
2016-05-07 20:38:12 +0200

    Update WinAwardController.php

--- gb/controller/WinAwardController.php
+++ gb/controller/WinAwardController.php
@@ -7,25 +7,35 @@
 namespace gb\controller;
 
 require_once("gb/controller/PageController.php");
-require_once("gb/mapper/Bookmapper.php");
+require_once("gb/mapper/BooksWithAwardsMapper.php");
 
 
 class WinAwardController extends PageController {
+	private $books;
     function process() {
         if (isset($_POST["search"])) {
             
 
 	if ((strlen($_POST["genre"]) > 0) &&
-                    (strlen($_POST["country"]) > 0) &&
-                    (strlen($_POST["from_time"]) > 0))&&
+                    (strlen($_POST["country_writer"]) > 0) &&
+                    (strlen($_POST["from_time"]) > 0)&&
                     (strlen($_POST["to_time"]) > 0))
 	   { 
 		// search books by genre, country, time
-		$this->books = $this->searchBooksByGenreCountryOfWriterAndTime($_POST["genre"],
-                                            $_POST["country_writer"], $_POST["time"]);
+		$this->books = $this-> getBooksByGenreAndCountryWithAward($_POST["genre"],
+                                            $_POST["country_writer"], $_POST["from_time"], $_POST["to_time"]);
                 
         }
     }
+	
+	}
+	
+	 function getBooksByGenreAndCountryWithAward($genre, $country_writer, $from_time, $to_time) {
+        $mapper = new \gb\mapper\BooksWithAwardsMapper();
+        return $mapper->getBooksByGenreAndCountryWithAward($genre, $country_writer, $from_time, $to_time);
+    }
+	function getSearchResult() {
+        return $this->books;
+    }
 }
-
 ?>
